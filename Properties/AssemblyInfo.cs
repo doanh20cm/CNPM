99f69564ad52f0efc712203f43ec64a166d4bbda
doanh20cm <doanhidm1@gmail.com>
2022-11-24 21:53:32 +0700

    pub ver 1.0.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,10 +1,11 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Quản lý nhân sự AnPhatComputer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Phần mềm quản lí nhân sự")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("doanh20cm")]
 [assembly: AssemblyProduct("Quản lý nhân sự")]
@@ -29,3 +30,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("vi")]
