e786453ef7d9c1093c312352a3a053cb86db90de
HuynhNhatLinh <nhatlinhgymer2002@gmail.com>
2022-12-23 00:46:12 +0700

    sua nut xuat excel form QKTaiKhoan

--- QLTaiKhoan.cs
+++ QLTaiKhoan.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -490,36 +491,29 @@ namespace Quan_li_nhan_su
                     {
                         // Tạo workbook mới
                         var workbook = new XLWorkbook();
+                        var table = KetNoi.GetData("select * from xemtk");
+                        workbook.Worksheets.Add(table, "Ds tài khoản");
 
-                        // Thêm một trang tính vào workbook
-                        var worksheet = workbook.Worksheets.Add("Sheet1");
-
-                        // Lấy phạm vi ô chứa dữ liệu cần xuất
-
-                        var range = worksheet.Range(1, 1, dgvTaiKhoan.RowCount + 1, dgvTaiKhoan.ColumnCount);
-
-                        // Đặt giá trị của hàng đầu tiên thành tên cột
-                        for (int i = 0; i < dgvTaiKhoan.ColumnCount; i++)
-                        {
-                            worksheet.Cell(1, i + 1).Value = dgvTaiKhoan.Columns[i].HeaderText;
-                        }
-                        // Đặt giá trị của phạm vi thành dữ liệu từ DataGridView
-                        for (int i = 0; i < dgvTaiKhoan.RowCount; i++)
-                        {
-                            for (int j = 0; j < dgvTaiKhoan.ColumnCount; j++)
-                            {
-                                worksheet.Cell(i + 2, j + 1).Value = dgvTaiKhoan.Rows[i].Cells[j].Value;
-                            }
-                        }
-
-                        // Lưu sổ file vào tệp do người dùng chỉ định
                         workbook.SaveAs(saveFileDialog.FileName);
+                        if (DialogResult.Yes == MessageBox.Show(
+                                @"Xuất dữ liệu thành công, bạn có muốn mở vị trí file không?",
+                                @"Thông báo", MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Question))
+                            using (var process = new Process
+                            {
+                                StartInfo = new ProcessStartInfo
+                                {
+                                    FileName = "explorer.exe",
+                                    Arguments = "/select," + saveFileDialog.FileName,
+                                    UseShellExecute = false
+                                }
+                            }) process.Start();
                     }
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Đã xảy ra lỗi khi xuất dữ liệu: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Đã xảy ra lỗi khi xuất dữ liệu", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
